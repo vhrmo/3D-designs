46e507760dee9e1162151acfa270a2a2f02b804b
Vladimir Hrmo <vladimir.hrmo@gmail.com>
2024-01-17 16:46:42 +0100

    Pridane projekty

---
+++ ArcWelder/postprocess.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# -g required for Marlin 2
+# --allow-3d-arcs required for Vase mode
+
+
+
+
+./ArcWelder *.gcode --allow-3d-arcs -r=0.6 -g
+
+
--- Projects/Odkvapkavac/postprocess.sh
+++ Projects/Odkvapkavac/postprocess.sh
@@ -7,4 +7,6 @@
 
 ../../ArcWelder/ArcWelder Odkvapkavac_2h26m_0.28mm_205C_PLA_ENDER3BLTOUCH.gcode 2_Odkvapkavac_2h26m_0.28mm_205C_PLA_ENDER3BLTOUCH.gcode --allow-3d-arcs -r=0.6 -g
 
+../../ArcWelder/ArcWelder Odkvapkavac_maly_1h10m_0.28mm_205C_PLA_ENDER3BLTOUCH.gcode 2_Odkvapkavac_maly_1h10m_0.28mm_205C_PLA_ENDER3BLTOUCH.gcode --allow-3d-arcs -r=0.6 -g
+
 
